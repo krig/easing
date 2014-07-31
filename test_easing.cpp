3ad5959c131fabd3d5d4a4ad2ab83220d54aab7f
Kristoffer Grönlund <krig@koru.se>
2014-07-31 14:22:55 +0200

    nicer readme

--- test_easing.cpp
+++ test_easing.cpp
@@ -8,7 +8,35 @@ using namespace std;
 using namespace easing;
 
 template <typename Fn>
+void plotline(Fn fn, char head, char filler, float y, int from, int to, float step,
+              vector<float> const& values, vector<char>& line) {
+	line.clear();
+	line.push_back(head);
+	bool hit = false;
+	for (int x = from; x < to; ++x) {
+		if (fabs(values[x] - y) <= step) {
+			hit = true;
+			line.push_back('*');
+		} else {
+			line.push_back(filler);
+		}
+	}
+	if (!hit) {
+		int minx = from;
+		for (int x = from + 1; x < to; ++x) {
+			if (fabs(values[x] - y) < fabs(values[minx] - y))
+				minx = x;
+		}
+		if (fabs(values[minx] - y) < step * 1.25)
+			line[minx + 1] = '*';
+	}
+	line.push_back(0);
+	cout << line.data() << endl;
+}
+
+template <typename Fn>
 void asciiplot(const char* name, Fn fn, int w, int h) {
+	vector<char> line;
 	vector<float> values;
 	float step_x = 1.f / (float) w;
 	float step_y = 1.f / (float) h;
@@ -25,58 +53,34 @@ void asciiplot(const char* name, Fn fn, int w, int h) {
 		}
 	}
 
-	vector<char> line;
-
 	if (pre_v > 1.f) {
 		int n = (int)((float)h * (pre_v - 1.f) + 1.f);
 		for (int y = 0; y < n; ++y) {
-			line.clear();
-			line.push_back(' ');
 			float fy = 1.f + (float)(n - y) / (float) h;
-			for (int x = 0; x < w; ++x) {
-				line.push_back((fabs(values[x] - fy) <= step_x) ? 'x' : ' ');
-			}
-			line.push_back('\0');
-			cout << line.data() << endl;
+			plotline(fn, ' ', ' ', fy, 0, w, step_x, values, line);
 		}
-	} else {
-		cout << endl;
 	}
+
 	for (int y = h; y >= 0; --y) {
-		line.clear();
-		line.push_back('|');
 		float fy = (float) y / (float) h;
-		for (int x = 0; x < w; ++x)
-			line.push_back((fabs(values[x] - fy) <= step_x) ? 'x' : ' ');
-		line.push_back('\0');
-		cout << line.data() << endl;
-	}
-	{
-		size_t nlen = strlen(name);
-		line.clear();
-		line.push_back('+');
-		for (int x = 0; x < w - nlen - 1; ++x)
-			line.push_back('-');
-		line.push_back(' ');
-		line.insert(line.end(), name, name + nlen);
-		line.push_back('\0');
-		cout << line.data() << endl;
+		plotline(fn, '|', ' ', fy, 0, w, step_x, values, line);
 	}
 	if (post_v < 0.f) {
 		int n = (int)((float)h * -post_v + 1.f);
 		for (int y = 0; y < n; ++y) {
-			line.clear();
-			line.push_back(' ');
 			float fy = (float)(-1 - y) / (float) h;
-			for (int x = 0; x < w; ++x) {
-				line.push_back((fabs(values[x] - fy) <= step_x) ? 'x' : ' ');
-			}
-			line.push_back('\0');
-			cout << line.data() << endl;
+			plotline(fn, y == 0 ? '+' : ' ', y == 0 ? '-' : ' ', fy, 0, w, step_x, values, line);
 		}
 	} else {
+		cout << '+';
+		for (int i = 0; i < w; ++i)
+			cout << '-';
 		cout << endl;
 	}
+	for (int i = 0; i < (w - strlen(name))/2; ++i)
+		cout << ' ';
+	cout << name << endl << endl;
+
 }
 
 #define TEST(fn) do { asciiplot(#fn, [](float f) { return fn(f); }, 60, 20); } while (0)
